8e02729b7ba0e2ed0428205a6c338df061d829f6
Goran A <alasov.jr@gmail.com>
2017-05-10 20:25:54 +0200

    Dodao sql

---
+++ AddStudents.sql
@@ -0,0 +1,5 @@
+insert into Studenti (id, ime, prezime, indeks, prosek) values
+	(1, 'Mitar', 'Miric', 'ind-1', 7.46),
+	(2, 'Seka', 'Aleksic', 'ind-543', 6.89),
+	(3, 'Sako', 'Polumenta', 'ind-66', 10.00),
+	(4, 'Haris', 'Dzinovic', 'ind-32', 8.90); 
---
+++ CreateTable.sql
@@ -0,0 +1,1 @@
+create table Studenti(id int primary key, ime nvarchar(50), prezime nvarchar(50), indeks nvarchar(50), prosek float);
